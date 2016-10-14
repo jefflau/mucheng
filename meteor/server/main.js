e4441bb6f12431b0206b9c2d2a74e052370c0444
Leon Talbert <leon.tabert@gmai.com>
2016-10-14 17:49:49 +0800

    basic api call

--- meteor/server/main.js
+++ meteor/server/main.js
@@ -1,10 +1,17 @@
 import { Meteor } from 'meteor/meteor';
-import 'whatwg-fetch'
+import 'fetch-everywhere'
 
 Meteor.startup(() => {
   // code to run on server at startup
 });
 
+function getRawData() {
+    
+    testUrl = 'https://www.cryptocompare.com/api/data/histoday/?e=CCCAGG&fsym=BTC&limit=93&tsym=USD';
 
+    return fetch(testUrl, {method: 'GET'}).then(res => console.dir(res.json().then(console.log))).catch(console.log);
+    
+}
 
+getRawData()
 
