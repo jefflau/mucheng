203fc9c60be2841615e05df8bef9f980346f53d8
Jeff Lau <jeff-lau@live.com>
2016-10-19 20:13:29 +0800

    Added spread

--- meteor/imports/bittrex.js
+++ meteor/imports/bittrex.js
@@ -1,7 +1,7 @@
 import 'fetch-everywhere'
 
-const getLatestOrders = (limit) =>
-  fetch(`https://bittrex.com/api/v1.1/public/getorderbook?market=BTC-NLG&type=both&depth=${limit}`)
+const getLatestOrders = (market, limit) =>
+  fetch(`https://bittrex.com/api/v1.1/public/getorderbook?market=${market}&type=both&depth=${limit}`)
     .then(res => res.json())
     .then(data => ({
       buy: data.result.buy.slice(0, limit),
@@ -20,14 +20,14 @@ const getMarketSummary = (market) =>
 const averager = (a, c) => a + c.Rate
 const weightedAverager = (a, c) => a + c.rate * c.weight
 
-const calcAverage = (limit) =>
-  getLatestOrders(limit).then(({ buy, sell }) => ({
+const calcAverage = (market, limit) =>
+  getLatestOrders(market, limit).then(({ buy, sell }) => ({
     buy: buy.reduce(averager, 0)/limit,
     sell: sell.reduce(averager, 0)/limit
   }))
 
-const calcWeightedAverage = (limit) =>
-  getLatestOrders(limit).then(({ buy, sell }) => {
+const calcWeightedAverage = (market, limit) =>
+  getLatestOrders(market, limit).then(({ buy, sell }) => {
     let totalVolumeB = buy.reduce((a, c) => a + c.Quantity, 0)
     let totalVolumeS = sell.reduce((a, c) => a + c.Quantity, 0)
     let buyWeight = buy.map(o => ({ weight: o.Quantity/totalVolumeB, rate: o.Rate}))
@@ -39,8 +39,8 @@ const calcWeightedAverage = (limit) =>
     }
   })
 
-const calculateOrderVolume = (limit) =>
-  getLatestOrders(limit).then(({ buy, sell }) => {
+const calculateOrderVolume = (market, limit) =>
+  getLatestOrders(market, limit).then(({ buy, sell }) => {
     let totalVolumeB = buy.reduce((a, c) => a + c.Quantity, 0)
     let totalVolumeS = sell.reduce((a, c) => a + c.Quantity, 0)
     let buyWeight = buy.map(o => ({ weight: o.Quantity/totalVolumeB, rate: o.Rate}))
@@ -52,7 +52,49 @@ const calculateOrderVolume = (limit) =>
     }
   })
 
+const sumOfLastOrders = (market, limit) =>
+  getLatestOrders(market, limit).then(({ buy, sell }) => {
+    return {
+      buy: buy.reduce((a, c) => a + c.Quantity * c.Rate, 0),
+      sell: sell.reduce((a, c) => a + c.Quantity * c.Rate, 0)
+    }
+  });
 
-// calcWeightedAverage(50).then(console.log)
-// calcAverage(50).then(console.log)
-getMarketSummary('btc-nlg').then(console.log)
+const calcSpread = (market) =>
+  getBidAsk(market).then(d => (d.ask - d.bid) / d.bid * 100)
+
+const getBidAsk = (market) =>
+  getMarketSummary(market).then(data => ({ bid: data.Bid, ask: data.Ask}))
+
+const marketSummary = (market, limit) => {
+  console.log(`Summary of ${market}`)
+  //getLatestOrders('btc-nlg', 10).then(console.log)
+  getBidAsk(market).then(data => {
+    console.log('Current Bid:', data.bid)
+    console.log('Current Ask:', data.ask)
+  })
+  sumOfLastOrders(market, limit).then(data => {
+    console.log(`Volume of last ${limit} bids`, data.buy, 'BTC')
+    console.log(`Volume of last ${limit} asks`, data.sell, 'BTC')
+  })
+  calcWeightedAverage(market, limit).then(data => {
+    console.log(`Weighted average of last ${limit} buy bids`, data.buy, 'BTC')
+    console.log(`Weighted average of last ${limit} sell asks`, data.sell, 'BTC')
+  })
+  calcSpread(market).then(spread => {
+    console.log('Spread: ', spread + '%')
+  })
+  // calcAverage('btc-nlg', limit).then((data) => {
+  //   console.log('Average of buy bids', data.buy, 'BTC')
+  //   console.log('Average of sell asks', data.sell, 'BTC')
+  // })
+  // getMarketSummary(market).then(console.log)
+}
+
+marketSummary('btc-nlg', 20)
+//
+// getLatestOrders('btc-nlg', 10)
+// sumOfLastOrders('btc-nlg', 10).then(console.log)
+// calcWeightedAverage('btc-nlg', 50).then(console.log)
+// calcAverage('btc-nlg', 50).then(console.log)
+// getMarketSummary('btc-nlg').then(console.log)
